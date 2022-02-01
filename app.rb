63d25c60951437150e68e756d4d1947132d47e57
David <dave_carroll82@hotmail.com>
2022-02-01 17:01:22 +0000

    @name query added

--- app.rb
+++ app.rb
@@ -1,11 +1,18 @@
 require 'sinatra'
 require "sinatra/reloader" if development?
-
+  
 get '/' do
   "Hello World"
 end
 
-get '/cat' do
+get '/random-cat' do
+  @name = ["Amigo", "Misty", "Almond"].sample
+  erb(:index)
+end
+
+get '/named-cat' do
+  p params
+  @name = params[:name]
   erb(:index)
 end
 
