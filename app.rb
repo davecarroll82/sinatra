7351f28bde712ab03e10f12bc725646bec00ba2e
David <dave_carroll82@hotmail.com>
2022-02-01 15:15:59 +0000

    added erb file

--- app.rb
+++ app.rb
@@ -2,9 +2,11 @@ require 'sinatra'
 require "sinatra/reloader" if development?
 
 get '/' do
-  "<div style='border: 3px dashed red'>
-    <img src='http://placekitten.com/500/500'>
-   </div>"
+  "Hello World"
+end
+
+get '/cat' do
+  erb(:index)
 end
 
 # get '/secret' do
