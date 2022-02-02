0431aa7e12d2a80d9315dba26a436ea09522efe4
David <dave_carroll82@hotmail.com>
2022-02-02 16:49:23 +0000

    cat project finished

--- app.rb
+++ app.rb
@@ -10,11 +10,16 @@ get '/random-cat' do
   erb(:index)
 end
 
-get '/named-cat' do
+post '/named-cat' do
   p params
   @name = params[:name]
   erb(:index)
 end
+
+get '/cat-form' do
+  erb(:cat_form)
+end
+
 
 # get '/secret' do
 #   "I did it my way"
